9b1022ac2904ca35ea0724662891416245392563
Frol8612 <infern00730@gmail.com>
2021-10-25 09:43:37 +0300

    fead: set config

--- scripts/docker/build-and-publish.sh
+++ scripts/docker/build-and-publish.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-REGION=us-west-1
-AWS_ACCOUNT_ID=
+REGION=eu-west-1
+AWS_ACCOUNT_ID=468847757722
 PROFILE=default
 #PROFILE=personalAccount
-DOCKER_APP_NAME=my-cart-api
 
-DOCKER_APP_REPOSITORY=my-cart-api
+DOCKER_APP_REPOSITORY=frol8612-cart-api
 AWS_DOCKER_REGISTRY_URL=$AWS_ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com
 DOCKER_TIME_TAG="$(date +%s)"
 DOCKER_LATEST_TAG=latest
@@ -16,7 +15,7 @@ docker logout
 # Login in AWS container registry with your AWS credentials
 aws ecr get-login-password --profile $PROFILE --region $REGION | docker login --username AWS --password-stdin $AWS_DOCKER_REGISTRY_URL
 # Build Docker image
-docker build -f "$(dirname "$0")/../../Dockerfiles/Dockerfile" -t $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" -t $DOCKER_APP_REPOSITORY:$DOCKER_LATEST_TAG "$(dirname "$0")/../../"
+docker build -f "$(dirname "$0")/../../Dockerfile" -t $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" -t $DOCKER_APP_REPOSITORY:$DOCKER_LATEST_TAG "$(dirname "$0")/../../"
 
 # Tag Docker image
 docker tag $DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG" $AWS_DOCKER_REGISTRY_URL/$DOCKER_APP_REPOSITORY:"$DOCKER_TIME_TAG"
